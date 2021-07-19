7ef4c93f7383631c450122442acb2f2b4a5afb69
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-19 17:04:36 -0500

    arrays and strings

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,37 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+
+            int[] numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            //INSTTRUCTIONS:
+            //1.Loop through the array and print out each value.
+
+            //2.Modify the loop to only print the odd numbers from the array.
+
+
+
+            //HOW DO I DO IT WITH A FOREACH LOOP?!?
+            foreach (int number in numbers)
+            {
+                
+                Console.WriteLine(number);
+
+            }
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            //this is printing 0 1 3 4 WTF?!?!
+
+
+
+
         }
     }
 }
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -7,6 +7,16 @@ namespace StringExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Strings!");
+
+            string sentence = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] arr1 = sentence.Split("");
+
+            Console.WriteLine(string.Join(",", arr1));
+
+            string[] arr2 = sentence.Split(". ");
+
+            Console.WriteLine(string.Join(". ", arr2));
         }
     }
 }
